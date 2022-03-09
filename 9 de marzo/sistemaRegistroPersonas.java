e4f85144d26c3228b65bd3c9044fa0308c89e8ae
Noelia <76636902+noeaversa@users.noreply.github.com>
2022-03-09 12:00:10 -0300

    Add files via upload

---
+++ 9 de marzo/sistemaRegistroPersonas.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class sistemaRegistroPersonas {
+    //private ArrayList<Character> ABM = new ArrayList<>();
+    private ArrayList<Persona> personas = new ArrayList<>();
+
+    public sistemaRegistroPersonas(Persona p){
+        personas.add(p);
+    }
+}
