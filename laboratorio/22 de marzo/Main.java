ec70d9a250d7c1a9430cfd7b5e8f715ad8429b73
Noelia <76636902+noeaversa@users.noreply.github.com>
2022-03-23 10:17:19 -0300

    Add files via upload

--- laboratorio/22 de marzo/Main.java
+++ laboratorio/22 de marzo/Main.java
@@ -4,34 +4,117 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int opciones1 = 0;
-        while(opciones1 < 3){
-            System.out.println("1. Ingrese Cliente");
-            System.out.println("2. Libro");
-            System.out.println("3. Sistema de libreria");
-            System.out.println("4. Salir");
-            Scanner ingresoOpciones = new Scanner(System.in);
-            opciones1 = ingresoOpciones.nextInt();
+        ArrayList<Libro> librosArray = new ArrayList<>();
+        ArrayList<Integer> cantidad = new ArrayList<>();
+        ArrayList<Cliente> clienteArray = new ArrayList<>();
+        ArrayList<Integer> idArray = new ArrayList<>();
+        Scanner ingresos;
+        String letra;
+        int opciones = 0;
+        System.out.println("quiere ingresar un libro? (1/2)");
+        ingresos = new Scanner(System.in);
+        opciones = ingresos.nextInt();
 
-            if(opciones1 == 1){
-                Scanner ingresos;
-                int id;
-                float edad;
-                String nombre;
-                ArrayList<Libro> libros;
-                System.out.println("Ingrese id");
-                ingresos = new Scanner(System.in);
-                id = ingresos.nextInt();
+        while (opciones == 1){
+            System.out.println("id: ");
+            ingresos = new Scanner(System.in);
+            int id = ingresos.nextInt();
 
-                System.out.println("edad:");
-                ingresos = new Scanner(System.in);
-                edad = ingresos.nextFloat();
+            System.out.println("nombre: ");
+            ingresos = new Scanner(System.in);
+            String nombre = ingresos.nextLine();
 
-                System.out.println("nombre");
-                ingresos = new Scanner(System.in);
-                nombre = ingresos.nextLine();
+            System.out.println("Precio: ");
+            ingresos = new Scanner(System.in);
+            float precio = ingresos.nextFloat();
+
+            System.out.println("Editorial: ");
+            ingresos = new Scanner(System.in);
+            String editorial = ingresos.nextLine();
+
+            Libro aux = new Libro(id, nombre, precio, editorial);
+            librosArray.add(aux);
+
+            System.out.println("quiere ingresar un libro mas? (1/2)");
+            ingresos = new Scanner(System.in);
+            opciones = ingresos.nextInt();
+        }
+
+        Cliente clienteAux = new Cliente(librosArray);
+        Cliente clienteAux1 = new Cliente(librosArray);
+        Cliente clienteAux3 = new Cliente(librosArray);
+
+        clienteArray.add(clienteAux);
+        clienteArray.add(clienteAux1);
+        clienteArray.add(clienteAux3);
+        SistemaLibreria libreria = new SistemaLibreria(clienteArray, librosArray, cantidad, idArray);
+
+        for(Cliente i: clienteArray){
+           if(i.getSize() > 10){
+               System.out.println("Cliente frecuente: ");
+               System.out.println(i.getNombre());
+           }
+        }
+
+
+        int counter = 0;
+        float auxiliarEdad = 0;
+
+        for (Cliente i : clienteArray){
+            for(Cliente j: clienteArray){
+                if(j.getEdad() == i.getEdad()){
+                    counter++;
+                    if(counter > 10 && auxiliarEdad != i.getEdad()){
+                        System.out.println("edad frecuente: ");
+                        System.out.println(i.getEdad());
+                        auxiliarEdad = i.getEdad();
+                        break;
+                        }
+                    }
+                }
+                counter = 0;
+            }
+
+        for (Libro i: librosArray){
+            if (i.getPrecio() > 5500.50){
+                System.out.println("Libro con precio mayor a $5500,50: " + i.getNombre());
             }
         }
 
+        System.out.println("ingrese una letra: ");
+        ingresos = new Scanner(System.in);
+        letra = ingresos.nextLine();
+
+
+        for (Libro i: librosArray){
+            String letraComparacion = i.getNombre();
+            if(letra.charAt(0) == letraComparacion.charAt(0)){
+                System.out.println(letraComparacion);
+            }
+        }
+
+
+        for (Libro i: librosArray){
+            if (i.getEditorial().equals("ElAteneo") || i.getEditorial().equals("Interzona") || i.getEditorial().equals("Sur y Alianza")){
+                System.out.println(i.getNombre() + " (" + i.getPrecio()/2 + ") ");
+            }
+        }
+
+        System.out.println("ingrese el id del libro: ");
+        ingresos = new Scanner(System.in);
+        int idLibroModificar = ingresos.nextInt();
+
+        System.out.println("ingrese el porcentaje de descuento: ");
+        ingresos = new Scanner(System.in);
+        int descuento = ingresos.nextInt();
+
+        for(Libro i: librosArray){
+            if(idLibroModificar == i.getId()){
+                float precioOriginal = i.getPrecio();
+                i.setPrecio(precioOriginal -((precioOriginal * descuento)/100));
+                System.out.println(i.getPrecio());
+            }
+        }
     }
 }
+
