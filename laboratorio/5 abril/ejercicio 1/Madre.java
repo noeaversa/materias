4a5f04cfaae780f29f5b59d4c0a362ba45abb654
Noelia <76636902+noeaversa@users.noreply.github.com>
2022-04-05 15:53:35 -0300

    Add files via upload

---
+++ laboratorio/5 abril/ejercicio 1/Madre.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Madre extends Persona{
+    private String nombreEmpresa;
+    private ArrayList<Hijo> hijos = new ArrayList();
+
+    public Madre(ArrayList<Hijo> hijos1){
+        super();
+        this.nombreEmpresa = "Telecentro :(";
+        this.hijos = hijos1;
+    }
+
+    public Madre(String nombre, int edad, int dni, int telefono, String direccion, String empresa, ArrayList<Hijo> hijos1){
+        super(nombre, edad, dni, telefono, direccion);
+        this.nombreEmpresa = empresa;
+        this.hijos = hijos1;
+    }
+
+    public ArrayList<Hijo> hijosMenores(){
+        ArrayList<Hijo> menores = new ArrayList();
+        for(Hijo i: hijos){
+            if(i.getEdad() < 18) menores.add(i);
+        }
+        return menores;
+    }
+}
