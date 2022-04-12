3f6cd06c24c540c5a7b056f06544c7b5c95ec8c2
Noelia <76636902+noeaversa@users.noreply.github.com>
2022-04-12 14:45:58 -0300

    Update Main.java

--- laboratorio/8 de marzo/ejercicio 1/Main.java
+++ laboratorio/8 de marzo/ejercicio 1/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 	Persona p1 = new Persona();
-        Persona p2 =new Persona("Lucas");
-        Persona p3= new Persona("Mila", 12, 23414141, 11717171, "Bazurco 33");
+        Persona p2 = new Persona("Lucas");
+        Persona p3 = new Persona("Mila", 12, 23414141, 11717171, "Bazurco 33");
 
         int edadP1 = p1.getEdad();
         p1.setEdad(edadP1 * 2);
