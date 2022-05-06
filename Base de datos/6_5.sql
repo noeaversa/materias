5cf17359e6a181870a49652cad7030cfaaaeca68
Noelia <76636902+noeaversa@users.noreply.github.com>
2022-05-06 17:43:02 -0300

    Create 6_5.sql

---
+++ Base de datos/6_5.sql
@@ -0,0 +1,25 @@
+create database compras;
+use compras;
+
+DELIMITER //
+
+-- 1:
+create procedure PrecioMayorAlPromedio(out cantidadProductos int)
+Begin
+    select * from products where buyPrice > avg(buyPrice);
+    select buyPrice into cantidadProductos from products where buyPrice > avg(buyPrice);
+    
+end //
+
+-- 2:
+create procedure existeElOrdenNumber(in orderNumberIn int, out filasOrderNumber int)
+Begin
+    select count(orderNumber) into filasOrderNumber from orders where orderNumberIn = orderNumber;
+    delete from orders where orderNumber = orderNumberIn;
+end //
+
+-- 3:
+create procedure borrarLinea()
+Begin
+
+DELIMITER ;
