57b69d912c95203862d735f20184caa4c3cce93a
Noelia <76636902+noeaversa@users.noreply.github.com>
2022-05-11 11:31:24 -0300

    Update Area.java

--- laboratorio/10_05/ejercicio_1/Area.java
+++ laboratorio/10_05/ejercicio_1/Area.java
@@ -1,24 +1,30 @@
 import static java.lang.Math.PI;
 public class Area {
-    public static double radio = 15;
-    public static double lado = 10;
+    public static void main(String[] args) {
+        double radio = 2, lado = 3, base = 4, altura = 5;
+        double circuloArea = CirculoArea(radio);
+        double esferaArea = EsferaArea(radio);
+        double cuadradoArea = CuadradoArea(lado);
+        double cuboArea = CuboArea(lado);
+        double trianguloArea = TrianguloArea(base, altura);
+    }
 
-    public static double CirculoArea(){
+    public static double CirculoArea(double radio){
         double area = PI * radio * radio;
         return area;
     }
 
-    public static double EsferaArea(){
+    public static double EsferaArea(double radio){
         double area = 4 * PI * radio * radio;
         return area;
     }
 
-    public static double CuadradoArea(){
+    public static double CuadradoArea(double lado){
         double area = lado * lado;
         return area;
     }
 
-    public static double CuboArea(){
+    public static double CuboArea(double lado){
         double area = 6 * lado * lado;
         return area;
     }
